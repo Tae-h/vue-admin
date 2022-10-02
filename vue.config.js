e75ca875d3bf43b32d1c13810ca01a6d4783948b
Tae-h <zppy13@gmail.com>
2022-10-02 17:52:23 +0900

    vuetify 적용

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,40 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+module.exports = {
+  runtimeCompiler: true,
+
+  configureWebpack: {
+    output: {
+      filename: 'js/[name].[hash:8].js',
+      chunkFilename: 'js/[name].js'
+    },
+  },
+
+  pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: false
+    },
+    rules: [
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            // Requires >= sass-loader@^8.0.0
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                indentedSyntax: true // optional
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
